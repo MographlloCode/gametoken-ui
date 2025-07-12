848d221ddb2f7348e8db21f002a08a93748f39d4
Gustavo Mello <GMELLOCONTACT@GMAIL.COM>
2025-07-12 06:15:40 -0300

    starting application ui dev

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from 'next/font/google'
+import "./globals.css";
+
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: "Game Token | Raise Your Games",
+  description: "Game Token | Raise Your Games",
+};
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased bg-[#111111] ${space_grotesk.className} h-screen w-screen overflow-hidden`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
